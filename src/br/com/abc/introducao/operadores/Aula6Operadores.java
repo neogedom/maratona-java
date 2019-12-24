d56fab6cd9c955fb4bdb30122e7817402e5aa4bc
Vinícius Gomes Ferreira <neogedom@gmail.com>
2019-12-24 09:58:49 -0300

    Comentando Aula6

--- src/br/com/abc/introducao/operadores/Aula6Operadores.java
+++ src/br/com/abc/introducao/operadores/Aula6Operadores.java
@@ -1,16 +1,17 @@
 package br.com.abc.introducao.operadores;
 
-public class Aula6Operadores{
-    public static void main (String args []) {
+public class Aula6Operadores {
+    public static void main(String args[]) {
         int numero1 = 10;
         int numero2 = 20;
 
-        
         System.out.println("A soma é: " + (numero1 + numero2));
         System.out.println("A multiplicação " + (numero1 * numero2));
         System.out.println("A subtração é: " + (numero1 - numero2));
-       
-        //Cuidado!
+
+        // Cuidado!
+        // Ao ter usado uma vez o + para concatenar, a próxima vez que ele for usado no
+        // sysout ele irá concatenar de novo (e não somar)
         System.out.println(numero1 + numero2 + " A soma é: " + numero1 + numero2);
         System.out.println("A divisão é: " + (numero1 / numero2));
 
